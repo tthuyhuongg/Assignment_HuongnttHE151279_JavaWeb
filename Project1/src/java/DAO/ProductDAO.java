e6ea58578a4c613932c90842960a5dadc1e48fa7
tthuyhuongg <97464839+tthuyhuongg@users.noreply.github.com>
2022-03-06 22:54:41 +0700

    Updated

---
+++ Project1/src/java/DAO/ProductDAO.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+
+import entity.Categories;
+import entity.Product;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author thuy huong
+ */
+public class ProductDAO extends BaseDAO<Product> {
+    PreparedStatement ps;
+    ResultSet rs;
+    @Override
+    public ArrayList<Product> getAll() {
+       ArrayList<Product> list = new ArrayList<>();
+       String sql="select* from Product";
+        try {
+            ps = connection.prepareStatement(sql);
+            rs = ps.executeQuery();
+            while(rs.next()){
+                Product p = new Product();
+                p.setMasp(rs.getString("masp"));
+                p.setNamesp(rs.getString("Name"));
+                p.setImage(rs.getString("Picture"));
+                p.setPrice(rs.getFloat("price"));
+                p.setTitle(rs.getString("title"));
+                p.setDescription(rs.getString("Description"));
+                list.add(p);
+            }
+        } catch (SQLException e) {
+        }
+        return list;
+       
+    }
+    public ArrayList<Categories> getAllCa() {
+       ArrayList<Categories> list = new ArrayList<>();
+       String sql="select* from Categories";
+        try {
+            ps = connection.prepareStatement(sql);
+            rs = ps.executeQuery();
+            while(rs.next()){
+                Categories c = new Categories();
+                c.setCaid(rs.getInt("CategoryID"));
+                c.setCaname(rs.getString("Cname"));
+                c.setImage(rs.getString("Picture"));
+                list.add(c);
+            }
+        } catch (SQLException e) {
+        }
+        return list;
+       
+    }
+    public ArrayList<Product> getHCM(String id) {
+       ArrayList<Product> list = new ArrayList<>();
+       String sql="select * from Product where Id=?";
+        try {
+            ps = connection.prepareStatement(sql);
+            ps.setString(1,id);
+            rs = ps.executeQuery();
+            while(rs.next()){
+                Product p = new Product();
+                p.setMasp(rs.getString("masp"));
+                p.setNamesp(rs.getString("Name"));
+                p.setImage(rs.getString("Picture"));
+                p.setPrice(rs.getFloat("price"));
+                p.setTitle(rs.getString("title"));
+                p.setDescription(rs.getString("Description"));
+                list.add(p);
+            }
+        } catch (SQLException e) {
+        }
+        return list;
+       
+    }
+    
+    
+}
---
+++ Project1/src/java/entity/Product.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+/**
+ *
+ * @author thuy huong
+ */
+public class Product {
+    private String masp;
+    private String namesp;
+    private String image;
+    private double price;
+    private String title;
+    private String description;
+
+    public Product() {
+    }
+
+    public Product(String masp, String namesp, String image, double price, String title, String description) {
+        this.masp = masp;
+        this.namesp = namesp;
+        this.image = image;
+        this.price = price;
+        this.title = title;
+        this.description = description;
+    }
+
+    public String getMasp() {
+        return masp;
+    }
+
+    public void setMasp(String masp) {
+        this.masp = masp;
+    }
+
+    public String getNamesp() {
+        return namesp;
+    }
+
+    public void setNamesp(String namesp) {
+        this.namesp = namesp;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" + "masp=" + masp + ", namesp=" + namesp + ", image=" + image + ", price=" + price + ", title=" + title + ", description=" + description + '}';
+    }
+   
+}
