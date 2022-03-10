f5e42216112816e53837539581ceaf227381d48b
tthuyhuongg <97464839+tthuyhuongg@users.noreply.github.com>
2022-03-10 23:21:54 +0700

    updated

--- Project1/src/java/DAO/ProductDAO.java
+++ Project1/src/java/DAO/ProductDAO.java
@@ -5,6 +5,7 @@
  */
 package DAO;
 
+import entity.Account;
 import entity.Categories;
 import entity.Product;
 import java.sql.PreparedStatement;
@@ -83,7 +84,7 @@ public class ProductDAO extends BaseDAO<Product> {
         } catch (SQLException e) {
         }
         return list;
-    }  
+    }
 
     public Product getsp(String msp) {
         String sql = "select * from Product where masp=?";
@@ -93,23 +94,23 @@ public class ProductDAO extends BaseDAO<Product> {
             rs = ps.executeQuery();
             while (rs.next()) {
                 return new Product(rs.getString("masp"),
-                rs.getString("Name"),
-                rs.getString("Picture"),
-                rs.getFloat("price"),
-                rs.getString("title"),
-                rs.getString("Description"));    
+                        rs.getString("Name"),
+                        rs.getString("Picture"),
+                        rs.getFloat("price"),
+                        rs.getString("title"),
+                        rs.getString("Description"));
             }
         } catch (SQLException e) {
         }
         return null;
     }
-    
+
     public ArrayList<Product> search(String txt) {
         ArrayList<Product> list = new ArrayList<>();
         String sql = "select * from Product where Name like ?";
         try {
             ps = connection.prepareStatement(sql);
-            ps.setString(1,"%"+ txt +"%");
+            ps.setString(1, "%" + txt + "%");
             rs = ps.executeQuery();
             while (rs.next()) {
                 Product p = new Product();
@@ -124,6 +125,55 @@ public class ProductDAO extends BaseDAO<Product> {
         } catch (SQLException e) {
         }
         return list;
-    }  
+    }
+
+    public Account login(String name, String pass) {
+        String sql = "select * from Acount where username =? and pass=?";
+        try {
+            ps = connection.prepareStatement(sql);
+            ps.setString(1, name);
+            ps.setString(2, pass);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                return new Account(rs.getInt(1),
+                        rs.getString(2),
+                        rs.getString(3),
+                        rs.getInt(4),
+                        rs.getInt(5));
+            }
+        } catch (Exception e) {
+        }
+        return null;
+
+    }
+
+    public Account checkacc(String name) {
+        String sql = "select * from Acount where username =?";
+        try {
+            ps = connection.prepareStatement(sql);
+            ps.setString(1, name);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                return new Account(rs.getInt(1),
+                        rs.getString(2),
+                        rs.getString(3),
+                        rs.getInt(4),
+                        rs.getInt(5));
+            }
+        } catch (Exception e) {
+        }
+        return null;
+    }
+
+    public void signup(String name, String pass) {
+        String sql = "insert into Acount values(?,?,1,0)";
+        try {
+            ps = connection.prepareStatement(sql);
+            ps.setString(1, name);
+            ps.setString(2, pass);
+            ps.executeUpdate();
+        } catch (Exception e) {
+        }
+    }
 
 }
